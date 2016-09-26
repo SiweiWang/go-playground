0720626a09de3c4496631a9bb18e3a81dc5dc924
siweiwang <wangsiwei2003@hotmail.com>
2016-09-25 23:38:50 -0400

    sychronization not safe

---
+++ sychronization_no_safe.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+var counter = 0
+
+func main() {
+  for i := 0; i < 20; i++ {
+    go incr()
+  }
+  time.Sleep(time.Millisecond * 10)
+}
+
+func incr() {
+  counter++
+  fmt.Println(counter)
+}
+
+// counter here is not thread(gorountine) safe. the value can container duplicated values.
